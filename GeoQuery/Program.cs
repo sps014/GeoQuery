52a22c01cd1d508a5f7e9ea454de4c55cdb07c03
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-03-26 14:20:01 +0530

    decoder

--- GeoQuery/Program.cs
+++ GeoQuery/Program.cs
@@ -6,7 +6,6 @@ namespace GeoQuery
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             var s = GeoQuery.Encode(new GeoPoint(125.676,18.8),12);
             var d = GeoQuery.Decode(s);
         }
