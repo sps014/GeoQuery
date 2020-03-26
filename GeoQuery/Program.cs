8238300eac6a06bc0d65ede193f55ac28ead807a
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-03-26 17:15:07 +0530

    Gustavo Niemeyer’s geocoding system

--- GeoQuery/Program.cs
+++ GeoQuery/Program.cs
@@ -7,6 +7,8 @@ namespace GeoQuery
         static void Main(string[] args)
         {
             var s = GeoQuery.Encode(26.83710, 80.92060, 6);
+            var sp = GeoQuery.Encode(new GeoPoint(26.83710, 80.92060), 6);
+
             var d = GeoQuery.Decode(s);
             var n1 = GeoQuery.Adjacent(s,Direction.North);
             var n4 = GeoQuery.Adjacent(s, Direction.East);
