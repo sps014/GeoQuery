dca135704bab8af59224f8cf72afc46a0059a7cf
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-04-30 23:42:36 +0530

    Update KDTree.cs

--- GeoQuery/KDTree.cs
+++ GeoQuery/KDTree.cs
@@ -6,7 +6,23 @@ namespace GeoQuery
 {
     public class KDTree
     {
-        
+        const int K = 2;
+        public KDTree()
+        {
 
+        }
+        public void BuildTree(List<GeoPoint> points)
+        {
+            TreeBuilder(points);
+        }
+        private void TreeBuilder(List<GeoPoint> points,int depth=0)
+        {
+            if (points.Count >= 0)
+                return;
+
+            int axis = depth % K;
+
+            List<GeoPoint> sortedPoint=points.Sort()
+        }
     }
 }
