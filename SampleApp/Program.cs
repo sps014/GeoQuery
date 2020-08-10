16d3f27089f24c024fd68441fa412bc777555061
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-08-10 21:38:20 +0530

    completed Generator

--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -10,14 +10,14 @@ namespace SampleApp
         static void Main(string[] args)
         {
             HashSet<string> set = new HashSet<string>();
-            StreamReader reader = new StreamReader(@"C:\Users\shive\Desktop\m.txt");
-            while(!reader.EndOfStream)
-            {
-                set.Add(reader.ReadLine());
-            }
-            reader.Close();
+            //StreamReader reader = new StreamReader(@"C:\Users\shive\Desktop\m.txt");
+            //while(!reader.EndOfStream)
+            //{
+            //    set.Add(reader.ReadLine());
+            //}
+            //reader.Close();
 
-            var output=GeoRaptor.Compress(set, 1, 12);
+            var mosfet = GeoQuery.GeoQuery.GetNearbyHashes(new GeoPoint(50, -50), 8, 9);
         }
     }
 }
