4c78793bfb0fbbd3e6923f5cf890b361bd2e5cfd
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-08-21 13:55:29 +0530

    removed blazor project dependency

--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -14,13 +14,13 @@ namespace SampleApp
             var radius = 5000;
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            _ = HashQuery.GetNearbyHashes(new GeoPoint(50, -50), radius, 8,false);
+            var hashes = HashQuery.GetNearbyHashes(new GeoPoint(50, -50), radius, 8,false);
 
             var v1 = stopwatch.ElapsedMilliseconds;
             stopwatch.Stop();
             Console.WriteLine("radius:  " + radius + " meters");
             Console.WriteLine("Time Taken :  " + v1 + " ms");
-
+            Console.WriteLine("Found: " + hashes.Count);
 
         }
     }
